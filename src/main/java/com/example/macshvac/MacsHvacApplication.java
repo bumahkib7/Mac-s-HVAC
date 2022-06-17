64aec68c4091bae7278e68d287db3eebe506a9c5
bumahkib7 <93518580+bumahkib7@users.noreply.github.com>
2022-06-17 11:39:48 +0200

    intial commit

---
+++ src/main/java/com/example/macshvac/MacsHvacApplication.java
@@ -0,0 +1,13 @@
+package com.example.macshvac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacsHvacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacsHvacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/macshvac/MacsHvacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.macshvac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacsHvacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
